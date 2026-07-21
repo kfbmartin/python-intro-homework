c85ac91ab95cd0338c1ed60ceb0f17b32f68b030
kfbmartin <your@email.com>
2026-07-21 14:10:53 -0400

    adding updates suggested by the AI Review 10

--- week-3/assignment-3/warmup2.py
+++ week-3/assignment-3/warmup2.py
@@ -2,13 +2,13 @@ age = int(input("Enter your age:"))
 
 if age>=0 and age<=12:
     #print(f"Your age is {age}.")
-    print("Child.")
+    print("You are a Child.")
 elif age>=13 and age<=17:
     #print(f"Your age is {age}.")
-    print("Teen.")
+    print("You are a Teen.")
 elif age>=18 and age<=64:
     #print(f"Your age is {age}.")
-    print("Adult.")
+    print("You are an Adult.")
 else:
     #print(f"Your age is {age}.")
-    print("Senior.")
+    print("You are a Senior.")
