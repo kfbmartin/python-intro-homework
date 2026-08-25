9e4eecf305f578d89b16b0f779e5ad341256a4af
kfbmartin <your@email.com>
2026-08-25 10:50:16 -0400

    added an exit function if the file wasn't found and remove the newline at the begining of the report

--- week-7/assignment-7/mini_project.py
+++ week-7/assignment-7/mini_project.py
@@ -6,13 +6,10 @@ from datetime import datetime
 #
 if not os.path.exists("../data/expenses.csv"):
     print("expenses.csv not found.")
+    exit()
 
 else: 
-    #with open('../data/expenses.csv', 'r') as file:
-       # reader = csv.DictReader(file)
-        #for row in reader:
-            #print(f"{row['date']}, {row['category']}, {row['description']}, {row['amount']}")
-
+  
 # Load all rows into memory
     with open('../data/expenses.csv', 'r') as file:
         records = list(csv.DictReader(file))
@@ -26,15 +23,12 @@ else:
     food_items = [item for item in records if item['category'] == 'Food']
    
     # Compute total inventory value
-    total_amount_spent = sum(item['amount'] * 1  for item in food_items)
+    total_amount_spent = sum(item['amount'] for item in food_items)
 
 now = datetime.now()
-#print(f"Food Expense Report — {now.strftime("%B, %d, %Y")}") 
-
-
 
 with open('food_report.txt', 'w') as file:
-    file.write(f"\nFood Expense Report — generated {now.strftime('%B %d, %Y')}")
+    file.write(f"Food Expense Report — generated {now.strftime('%B %d, %Y')}")
     for item in food_items:
         file.write(f"\n{item['date']}: ${item['amount']:.2f}")
 
