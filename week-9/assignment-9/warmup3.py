92d5e33185e9b061efdf54b3d7ad67087b3c1c91
kfbmartin <your@email.com>
2026-09-01 22:31:22 -0400

    removed the API_KEY from the code

--- week-9/assignment-9/warmup3.py
+++ week-9/assignment-9/warmup3.py
@@ -1,8 +1,14 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("RC_KEY")
 
 response = requests.get(
   'https://api.restcountries.com/countries/v5?region=Europe&response_fields=names.common&limit=10',
-  headers={'Authorization': 'Bearer rc_live_263a59a4f1a040cbafdfa9c16f183edc'}
+  headers={'Authorization': f"Bearer {API_KEY}"}
 )
 
 response_data = response.json()
