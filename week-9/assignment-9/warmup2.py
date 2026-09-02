9cd29ece5d5f213d53c960c6735598f185f82f9b
kfbmartin <your@email.com>
2026-09-01 20:58:25 -0400

    fixing the fall error message for birthday

--- week-9/assignment-9/warmup2.py
+++ week-9/assignment-9/warmup2.py
@@ -3,12 +3,10 @@ import requests
 response = requests.get("https://api.agify.io/?name=michael")
 response_json = response.json()
 
-birthday_key = response_json.get("birthday")
-birthday = birthday_key[0] if birthday_key else "Not available"
-
+birthday = response_json.get("birthday", "Not available")
 
 print(f"Name: {response_json["name"]}")
-print(f"Predicted age: {response_json["age"]} ")
+print(f"Predicted age: {response_json["age"]}")
 print(f"Birthday: {birthday}")
 
 
