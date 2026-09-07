d4b5c9492ea190ddd5a9a56e0484d74fb95c1c5a
kfbmartin <your@email.com>
2026-09-06 20:19:59 -0400

    updating the error text to match the assignment

--- week-9/assignment-9/warmup4.py
+++ week-9/assignment-9/warmup4.py
@@ -5,10 +5,12 @@ try:
     response = requests.get(url)
 
     if response.status_code != 200:
-        print(f"Request failed: status {response.status_code}")
+        print(f"Error: Request failed with status code {response.status_code}")
         
     else:
         data = response.json()
+        print(data)
+
         # work with data here
-except requests.exceptions.RequestException as e:
-    print(f"Error: Could not reach the server. Check your connection and try again.")
+except requests.exceptions.RequestException:
+    print("Error: Could not reach the server. Check your connection and try again.")
